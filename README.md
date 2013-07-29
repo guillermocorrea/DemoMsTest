21a8463cf690715d180d2877e30f5e446335814b
Guillermo Correa <guille_freelance@hotmail.com>
2013-07-29 08:52:05 -0500

    Added solution

---
+++ README.md
@@ -0,0 +1,5 @@
+DemoMsTest
+==========
+
+Dummy solution that implements unit tests using MsTest, also includes a .msbuild file with targets for build, test, 
+cobertura analisys.
---
+++ src/DemoMsTest.Logic/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoMsTest.Logic
+{
+    public static class Calculator
+    {
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
